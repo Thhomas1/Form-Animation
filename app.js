864121de25bc829b18d066f6cfb62493fcb9b083
Thhomas1 <thomasromero10@hotmail.com>
2023-02-04 05:00:05 -0300

    Elastic Effect PlaceHolder

---
+++ app.js
@@ -0,0 +1,31 @@
+const containers = document.querySelectorAll('.input-container');
+const form = document.querySelectorAll('form');
+
+const tl = gsap.timeline({ defaults: {duration: 1}});
+
+
+// linea
+
+const start = 
+"M0 0.999512C0 0.999512 60.5 0.999512 150 0.999512C239.5 0.999512 300 0.999512 300 0.999512";
+
+const end = 
+"M1 0.999512C1 0.999512 61.5 7.5 151 7.5C240.5 7.5 301 0.999512 301 0.999512";
+
+
+// elastic effect
+
+containers.forEach(container => {
+    const input = container.querySelector('.input');
+    const line = container.querySelector('.elastic-line');
+    const placeholder = container.querySelector('.placeholder');
+
+
+    input.addEventListener('click', () => {
+        // este if verifica si esta vacio el input
+        if(!input.value){
+            tl.fromTo(line, {attr: {d: start}}, {attr: {d: end}, ease: 'Power2.easeOut', duration: 0.75}
+            );
+        }
+    });
+});
